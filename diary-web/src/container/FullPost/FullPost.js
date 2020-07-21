7a34520d1112acb16593128af6f04f234fe6d2e5
impulse <aman1122singh.as@gmail.com>
2020-07-21 23:41:12 +0530

    Initial Basic CRUD

--- diary-web/src/container/FullPost/FullPost.js
+++ diary-web/src/container/FullPost/FullPost.js
@@ -3,54 +3,55 @@ import axios from "../../axios-posts";
 import Button from "../../components/UI/Forms/Button/Button";
 import { Redirect } from "react-router-dom";
 class FullPost extends Component {
-  state = {
-    loadedPost: null,
-    redirect: false,
-  };
-  componentDidMount() {
-    axios
-      .get("/posts/" + this.props.match.params.id + ".json")
-      .then((res) => {
-        // console.log(res.data);
-        this.setState({ loadedPost: res.data });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-  deletePostHandler = () => {
-    axios
-      .delete("/posts/" + this.props.match.params.id + ".json")
-      .then((res) => {
-        // console.log(res.data);
-        // this.setState({ loadedPost: res.data });
-        this.setState({ redirect: true });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  editPostHandler = () => {
-    this.props.history.push(this.props.location.pathname + "/edit");
-  };
-  render() {
-    let post = null;
-    if (this.state.loadedPost) {
-      post = (
-        <div>
-          <p>Title: {this.state.loadedPost.title}</p>
-          <p>Content: {this.state.loadedPost.content}</p>
-          <Button clicked={this.deletePostHandler}>Delete</Button>
-          <Button clicked={this.editPostHandler}>Edit</Button>
-        </div>
-      );
+    state = {
+        loadedPost: null,
+        redirect: false,
+    };
+    componentDidMount() {
+        axios
+            .get("/posts/" + this.props.match.params.id + ".json")
+            .then((res) => {
+                console.log(res);
+                this.setState({ loadedPost: res.data });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
-    if (this.state.redirect) {
-      post = <Redirect to="/" />;
-    }
+    deletePostHandler = () => {
+        axios
+            .delete("/posts/" + this.props.match.params.id + ".json")
+            .then((res) => {
+                // console.log(res.data);
+                // this.setState({ loadedPost: res.data });
+                this.setState({ redirect: true });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+    editPostHandler = () => {
+        this.props.history.push(this.props.location.pathname + "/edit");
+    };
+    render() {
+        let post = null;
+        if (this.state.loadedPost) {
+            post = ( <
+                div >
+                <
+                p > Title: { this.state.loadedPost.title } < /p> <
+                p > Content: { this.state.loadedPost.content } < /p> <
+                Button clicked = { this.deletePostHandler } > Delete < /Button> <
+                Button clicked = { this.editPostHandler } > Edit < /Button> <
+                /div>
+            );
+        }
+        if (this.state.redirect) {
+            post = < Redirect to = "/" / > ;
+        }
 
-    return post;
-  }
+        return post;
+    }
 }
 
 export default FullPost;
--- diary-web/src/container/Posts/Posts.js
+++ diary-web/src/container/Posts/Posts.js
@@ -5,42 +5,45 @@ import { Link } from "react-router-dom";
 import Auxillary from "../../hoc/Auxillary/Auxillary";
 import Button from "../../components/UI/Forms/Button/Button";
 class Posts extends Component {
-  state = {
-    posts: null,
-  };
+    state = {
+        posts: null,
+    };
 
-  componentDidMount() {
-    Axios.get("/posts.json")
-      .then((res) => {
-        let fetchedPosts = [];
-        for (let key in res.data) {
-          fetchedPosts.push({ ...res.data[key], id: key });
-        }
-        this.setState({ posts: fetchedPosts });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  render() {
-    let postRender = null;
-    if (this.state.posts) {
-      postRender = this.state.posts.map((post) => (
-        <Link to={"/post/" + post.id} key={post.id}>
-          <Post postData={post} />
-        </Link>
-      ));
+    componentDidMount() {
+        Axios.get("/posts.json")
+            .then((res) => {
+                let fetchedPosts = [];
+                for (let key in res.data) {
+                    fetchedPosts.push({...res.data[key], id: key });
+                }
+                this.setState({ posts: fetchedPosts });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
-    return (
-      <Auxillary>
-        {postRender}
-        <Link to="/new">
-          <Button>New Post</Button>
-        </Link>
-      </Auxillary>
-    );
-  }
+
+    render() {
+        let postRender = null;
+        if (this.state.posts) {
+            postRender = this.state.posts.map((post) => ( <
+                Link to = { "/post/" + post.id }
+                key = { post.id } >
+                <
+                Post postData = { post }
+                /> <
+                /Link>
+            ));
+        }
+        return ( <
+            Auxillary > { postRender } <
+            Link to = "/new" >
+            <
+            Button > New Post < /Button> <
+            /Link> <
+            /Auxillary>
+        );
+    }
 }
 
 export default Posts;
